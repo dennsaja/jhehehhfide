4cc78b7a229b04a94724927358a56d504a0372aa
Denis <136689391+dennsaja@users.noreply.github.com>
2025-12-25 14:11:11 +0700

    Create qr.js

---
+++ pages/api/qr.js
@@ -0,0 +1,22 @@
+import QRCode from "qrcode"
+
+export default async function handler(req, res) {
+  const { id } = req.query
+
+  if (!id) {
+    return res.status(400).send("ID kabel wajib")
+  }
+
+  const url = `https://data.infinityteknik.net/kabel/${id}`
+
+  try {
+    const qr = await QRCode.toDataURL(url, {
+      width: 300,
+      margin: 2
+    })
+
+    res.status(200).json({ qr })
+  } catch (err) {
+    res.status(500).send("Gagal membuat QR")
+  }
+}
